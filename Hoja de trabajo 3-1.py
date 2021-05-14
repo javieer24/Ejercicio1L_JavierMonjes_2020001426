107d521a73902fd70cb0d8c529dbd859bdada348
JAVIER <javiermonjesnuevo@gmail.com>
2021-05-14 15:36:05 -0600

    Hoja de trabajo3

---
+++ Hoja de trabajo 3-1.py
@@ -0,0 +1,5 @@
+#  Número positivo cuenta hacia atras asta 0
+n = int(input("Introduce un número entero positivo: "))
+for i in range(n, -1, -1):
+    print(i, end=", ")
+    
---
+++ Hoja de trabajo 3.py
@@ -0,0 +1,7 @@
+# Número entero y triangulo 
+
+n = int(input("Introduce la altura del triángulo (entero positivo): "))
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print("")
